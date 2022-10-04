e72a0f0b56ce0486392092a214ebe6d8570a009f
zell <m.naghizadeh91@gmail.com>
2022-10-04 13:54:23 +0200

    update opengv

--- ports/opengv/portfile.cmake
+++ ports/opengv/portfile.cmake
@@ -4,7 +4,7 @@ set(opengv_VERSION 1.0)
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/laurentkneip/opengv/archive/master.zip"
     FILENAME "opengv-master.zip"
-    SHA512 0
+    SHA512 06e38dafe31e35037695ca3ce0a07296b3adcb079b08115c113135788f0047de0bb10ee51adbd5595d17e934cacafa6498b954584e07b721b5da8c4decd2ab3b
 )
 
 vcpkg_extract_source_archive_ex(
@@ -26,7 +26,7 @@ vcpkg_install_cmake()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 # Install CMake package definitions
-file(WRITE ${CURRENT_PACKAGES_DIR}/share/opengv/opengvConfig.cmake "
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/opengv/opengvConfig.cmake")
 # This script sets following variables:
 # OPENGV_FOUND
 # OPENGV_INCLUDE_DIR
